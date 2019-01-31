d70cff6485c86ee39ce5925c8a5903a4dfb58b10
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-01-31 10:05:17 -0500

    Now using Webpack loaders instead of NPM scripts for build processes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,81 @@
-module.exports = {
-    entry: {
-        index: "./dist/scripts/index.js"
-    },
-    externals: {
-        THREE: "three"
-    },
-    output: {
-        path: __dirname + "/dist/bundles/",
-        filename: "[name].js"
-    }
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin  = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const eslintrc = require("./.eslintrc.js");
+const babelrc  = require("./.babelrc.js");
+
+module.exports = (env, argv) => {
+    const devMode = argv.mode === "development";
+
+    return {
+        entry: {
+            index: "./src/index/index.js"
+        },
+        externals: {
+            three: "THREE"
+        },
+        output: {
+            path: __dirname + "/dist/",
+            filename: "[name]-[contenthash].js"
+        },
+
+        devtool: devMode ? 
+            "eval-source-map" :     // "best quality SourceMaps for development", see https://webpack.js.org/configuration/devtool/#development
+            "hidden-source-map",    // "Useful if you only want SourceMaps to map error stack traces from error reports", see https://webpack.js.org/configuration/devtool/#production
+
+        module: {
+            rules: [
+
+                // Markup rules
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: "html-loader",
+                        options: { minimize: !devMode }
+                    }]
+                },
+
+                // Stylesheet rules
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: MiniCssExtractPlugin.loader },
+                        { loader: "css-loader", options: { sourceMap: true } }
+                    ]
+                },
+
+                // Script rules
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [
+                        { loader: "babel-loader",  options: babelrc },
+                        { loader: "eslint-loader", options: eslintrc }
+                    ]
+                }
+            ]
+        },
+
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new OptimizeCSSAssetsPlugin({})
+            ]
+        },
+
+        plugins: [
+            new CleanWebpackPlugin(["dist"]),   // Deletes the output folder before each run
+            new HtmlWebpackPlugin({
+                template: "src/index/index.html",
+                filename: "index.html",
+                minify: !devMode,
+                cache: true    // Emit the file only if it was changed
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name]-[contenthash].css"
+            })
+        ]
+    };
 };
